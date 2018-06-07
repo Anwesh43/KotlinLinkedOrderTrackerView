706d0662341ecb2ccc56ce8f6c8380369aec723b
Anwesh43 <anweshthecool0@gmail.com>
2018-06-07 11:16:25 +0530

    adding Activity in View

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedordertrackerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedordertrackerview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedordertrackerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedordertrackerview.LinkedOrderTrackerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedOrderTrackerView.create(this)
     }
 }

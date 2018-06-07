7dc3c6b7d9563f3f5e7825c7346f698ed6ff67ff
Anwesh43 <anweshthecool0@gmail.com>
2018-06-07 10:11:58 +0530

    adding linkedordertrackerview module

---
+++ linkedordertrackerview/src/main/java/com/anwesh/uiprojects/linkedordertrackerview/LinkedOrderTrackerView.kt
@@ -0,0 +1,10 @@
+package com.anwesh.uiprojects.linkedordertrackerview
+
+/**
+ * Created by anweshmishra on 07/06/18.
+ */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
